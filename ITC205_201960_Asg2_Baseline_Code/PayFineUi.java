632cbba0dc7861358efcdca896b79b9fcac6cfc8
Kenneth <bashemlots@gmail.com>
2019-08-23 15:50:40 +1000

    last 4 finished

--- ITC205_201960_Asg2_Baseline_Code/PayFineUi.java
+++ ITC205_201960_Asg2_Baseline_Code/PayFineUi.java
@@ -1,83 +1,86 @@
 import java.util.Scanner;
 
 public class PayFineUi {
-	public static enum UiState { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
-	private PayFineControl paymentControl;	
-	private Scanner inputScanner;
-	private UiState uiState;
-	
-	public PayFineUi(PayFineControl newPayment) {
-		this.paymentControl = newPayment;
-		inputScanner = new Scanner(System.in);
-		uiState = UiState.INITIALISED;
-		newPayment.setUi(this);
-	}
-		
-	public void setState(UiState state) {
-		this.uiState = state;
-	}
+    public static enum UiState { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
+    private PayFineControl paymentControl;	
+    private Scanner inputScanner;
+    private UiState uiState;
 
-	public void run() {
-		output("Pay Fine Use Case UI\n");
-		while (true) {
-			switch (uiState) {
-			case READY:
-				String memberCardRead = input("Swipe member card (press <enter> to cancel): ");
-				if (memberCardRead.length() == 0) {
-					paymentControl.cancel();
-					break;
-				}
-				try {
-					int memberId = Integer.valueOf(memberCardRead).intValue();
-					paymentControl.cardSwiped(memberId);
-				}
-				catch (NumberFormatException e) {
-					output("Invalid memberId");
-				}
-				break;
-				
-			case PAYING:
+    public PayFineUi(PayFineControl newPayment) {
+        this.paymentControl = newPayment;
+        inputScanner = new Scanner(System.in);
+        uiState = UiState.INITIALISED;
+        newPayment.setUi(this);
+    }
 
-				double paymentAmount = 0;
-				String newPaymentAmount = input("Enter amount (<Enter> cancels) : ");
-				if (newPaymentAmount.length() == 0) {
-					paymentControl.cancel();
-					break;
-				}
-				try {
-					paymentAmount = Double.valueOf(newPaymentAmount).doubleValue();
-				}
-				catch (NumberFormatException e) {}
-				if (paymentAmount <= 0) {
-					output("Amount must be positive");
-					break;
-				}
-				paymentControl.payFine(paymentAmount);
-				break;
-								
-			case CANCELLED:
-				output("Pay Fine process cancelled");
-				return;			
-			case COMPLETED:
-				output("Pay Fine process complete");
-				return;			
-			default:
-				output("Unhandled state");
-				throw new RuntimeException("FixBookUI : unhandled state :" + uiState);			
-			}		
-		}		
-	}
-	
-	private String input(String prompt) {
-		System.out.print(prompt);
-		return inputScanner.nextLine();
-	}	
-		
-	private void output(Object object) {
-		System.out.println(object);
-	}	
+    public void setState(UiState state) {
+        this.uiState = state;
+    }
 
-	public void display(Object object) {
-		output(object);
-	}
+    public void run() {
+        output("Pay Fine Use Case UI\n");
+        while (true) {
+            switch (uiState) {
+            case READY:
+                String memberCardRead = input("Swipe member card (press <enter> to cancel): ");
+                int cardNumberLength = memberCardRead.length();
+                if (cardNumberLength == 0) {
+                    paymentControl.cancel();
+                    break;
+                }
+                try {
+                    int memberId = Integer.valueOf(memberCardRead).intValue();
+                    paymentControl.cardSwiped(memberId);
+                }
+                catch (NumberFormatException e) {
+                    output("Invalid memberId");
+                }
+                break;
+
+            case PAYING:
+                double paymentAmount = 0;
+                String newPaymentAmount = input("Enter amount (<Enter> cancels) : ");
+                int paymentAmountLength = newPaymentAmount.length();
+                if (paymentAmountLength == 0) {				    
+                    paymentControl.cancel();
+                    break;
+                }
+                try {
+                    paymentAmount = Double.valueOf(newPaymentAmount).doubleValue();
+                }
+                catch (NumberFormatException e) {}
+                if (paymentAmount <= 0) {
+                    output("Amount must be positive");
+                    break;
+                }
+                paymentControl.payFine(paymentAmount);
+                break;
+
+            case CANCELLED:
+                output("Pay Fine process cancelled");
+                return;
+
+            case COMPLETED:
+                output("Pay Fine process complete");
+                return;	
+
+            default:
+                output("Unhandled state");
+                throw new RuntimeException("FixBookUI : unhandled state :" + uiState);			
+            }		
+        }		
+    }
+
+    private String input(String prompt) {
+        System.out.print(prompt);
+        return inputScanner.nextLine();
+    }	
+
+    private void output(Object object) {
+        System.out.println(object);
+    }	
+
+    public void display(Object object) {
+        output(object);
+    }
 }
--- ITC205_201960_Asg2_Baseline_Code/ReturnBookControl.java
+++ ITC205_201960_Asg2_Baseline_Code/ReturnBookControl.java
@@ -1,67 +1,73 @@
 public class ReturnBookControl {
-	private ReturnBookUi returnBookUi;
-	private enum ControlState { INITIALISED, READY, INSPECTING };
-	private ControlState bookReturnState;
-	private Library library;
-	private Loan currentLoan;
+    private ReturnBookUi returnBookUi;
+    private enum ControlState { INITIALISED, READY, INSPECTING };
+    private ControlState bookReturnState;
+    private Library library;
+    private Loan currentLoan;
 
-	public ReturnBookControl() {
-		this.library = Library.instance();
-		bookReturnState = ControlState.INITIALISED;
-	}
-	
-	public void setUi(ReturnBookUi ui) {
-		if (!bookReturnState.equals(ControlState.INITIALISED)) {
-			throw new RuntimeException("ReturnBookControl: cannot call setUI except in INITIALISED state");
-		}	
-		this.returnBookUi = ui;
-		ui.setState(ReturnBookUi.UiState.READY);
-		bookReturnState = ControlState.READY;		
-	}
+    public ReturnBookControl() {
+        this.library = Library.instance();
+        bookReturnState = ControlState.INITIALISED;
+    }
 
-	public void bookScanned(int bookId) {
-		if (!bookReturnState.equals(ControlState.READY)) {
-			throw new RuntimeException("ReturnBookControl: cannot call bookScanned except in READY state");
-		}	
-		Book currentBook = library.book(bookId);
-		if (currentBook == null) {
-			returnBookUi.display("Invalid Book Id");
-			return;
-		}
-		if (!currentBook.onLoan()) {
-			returnBookUi.display("Book has not been borrowed");
-			return;
-		}		
-		currentLoan = library.loanByBookId(bookId);	
-		double overDueFine = 0.0;
-		if (currentLoan.isOverdue()) {
-			overDueFine = library.calculateOverdueFine(currentLoan);
-		}
-		returnBookUi.display("Inspecting");
-		returnBookUi.display(currentBook.toString());
-		returnBookUi.display(currentLoan.toString());
-		
-		if (currentLoan.isOverdue()) {
-			returnBookUi.display(String.format("\nOverdue fine : $%.2f", overDueFine));
-		}
-		returnBookUi.setState(ReturnBookUi.UiState.INSPECTING);
-		bookReturnState = ControlState.INSPECTING;		
-	}
+    public void setUi(ReturnBookUi ui) {
+        Boolean isIntialised = bookReturnState.equals(ControlState.INITIALISED);
+        if (!isIntialised) {
+            throw new RuntimeException("ReturnBookControl: cannot call setUI except in INITIALISED state");
+        }	
+        this.returnBookUi = ui;
+        ui.setState(ReturnBookUi.UiState.READY);
+        bookReturnState = ControlState.READY;		
+    }
 
-	public void scanningComplete() {
-		if (!bookReturnState.equals(ControlState.READY)) {
-			throw new RuntimeException("ReturnBookControl: cannot call scanningComplete except in READY state");
-		}	
-		returnBookUi.setState(ReturnBookUi.UiState.COMPLETED);		
-	}
+    public void bookScanned(int bookId) {
+        Boolean isReady = bookReturnState.equals(ControlState.READY);
+        if (!isReady) {
+            throw new RuntimeException("ReturnBookControl: cannot call bookScanned except in READY state");
+        }	
+        Book currentBook = library.book(bookId);
+        if (currentBook == null) {
+            returnBookUi.display("Invalid Book Id");
+            return;
+        }
+        Boolean inOnLoan = currentBook.onLoan();
+        if (!inOnLoan) {
+            returnBookUi.display("Book has not been borrowed");
+            return;
+        }		
+        currentLoan = library.loanByBookId(bookId);	
+        double overDueFine = 0.0;
+        Boolean isOverdue = currentLoan.isOverdue();
+        if (isOverdue) {
+            overDueFine = library.calculateOverdueFine(currentLoan);
+        }
+        returnBookUi.display("Inspecting");
+        returnBookUi.display(currentBook.toString());
+        returnBookUi.display(currentLoan.toString());
+        if (isOverdue) {
+            String fineString = String.format("\nOverdue fine : $%.2f", overDueFine);
+            returnBookUi.display(fineString);
+        }
+        returnBookUi.setState(ReturnBookUi.UiState.INSPECTING);
+        bookReturnState = ControlState.INSPECTING;		
+    }
 
-	public void dischargeLoan(boolean isDamaged) {
-		if (!bookReturnState.equals(ControlState.INSPECTING)) {
-			throw new RuntimeException("ReturnBookControl: cannot call dischargeLoan except in INSPECTING state");
-		}	
-		library.dischargeLoan(currentLoan, isDamaged);
-		currentLoan = null;
-		returnBookUi.setState(ReturnBookUi.UiState.READY);
-		bookReturnState = ControlState.READY;				
-	}
+    public void scanningComplete() {
+        boolean isReady = bookReturnState.equals(ControlState.READY);
+        if (!isReady) {
+            throw new RuntimeException("ReturnBookControl: cannot call scanningComplete except in READY state");
+        }	
+        returnBookUi.setState(ReturnBookUi.UiState.COMPLETED);		
+    }
+
+    public void dischargeLoan(boolean isDamaged) {
+        boolean isInspecting = bookReturnState.equals(ControlState.INSPECTING);
+        if (!isInspecting) {
+            throw new RuntimeException("ReturnBookControl: cannot call dischargeLoan except in INSPECTING state");
+        }	
+        library.dischargeLoan(currentLoan, isDamaged);
+        currentLoan = null;
+        returnBookUi.setState(ReturnBookUi.UiState.READY);
+        bookReturnState = ControlState.READY;				
+    }
 }
